3b4b4a4e321e5185b11cb4ef1a25ffc120d73fc4
Marek Fajkus <marek.faj@gmail.com>
2020-03-07 20:43:17 +0100

    configure nix ghcjs support

--- default.nix
+++ default.nix
@@ -9,6 +9,14 @@ let
         };
     };
   };
-  pkgs =
-    import <nixpkgs> { inherit config; };
-in pkgs.haskellPackages.aeson-combinators
+  pkgs = import <nixpkgs> { inherit config; };
+
+  # Using miso's ghcjs cache
+  ghcjs = import (builtins.fetchTarball {
+    url = "https://github.com/dmjio/miso/archive/561ffad.tar.gz";
+    sha256 = "1wwzckz2qxb873wdkwqmx9gmh0wshcdxi7gjwkba0q51jnkfdi41";
+  }) {};
+in {
+  ghc = pkgs.haskellPackages.aeson-combinators;
+  ghcjs = ghcjs.pkgs.haskell.packages.ghcjs.callCabal2nix "aeson-combinators" ./. {};
+}
