73fb6bea0e8dd2c5330242a941d090d5d4a8168e
Marek Fajkus <marek.faj@gmail.com>
2020-03-07 21:21:07 +0100

    better comment

--- default.nix
+++ default.nix
@@ -11,7 +11,9 @@ let
   };
   pkgs = import <nixpkgs> { inherit config; };
 
-  # Using miso's ghcjs cache
+  # Using miso's ghcjs
+  # cachix use miso-haskell
+  # see https://github.com/dmjio/miso/blob/e222a66566c0377738791ab563054bbfbe6abd15/README.md#nix
   ghcjs = import (builtins.fetchTarball {
     url = "https://github.com/dmjio/miso/archive/561ffad.tar.gz";
     sha256 = "1wwzckz2qxb873wdkwqmx9gmh0wshcdxi7gjwkba0q51jnkfdi41";
