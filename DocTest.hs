6c834504a27580d78663aaebd8c82dee4d47501b
Fendor <power.walross@gmail.com>
2022-03-19 11:27:59 +0100

    Update doctest

--- DocTest.hs
+++ DocTest.hs
@@ -1,6 +1,8 @@
 import Test.DocTest
-main = doctest [ "-isrc"
-               , "-XOverloadedStrings"
+main = doctest [ "-XOverloadedStrings"
+               , "-XCPP"
+               , "-XLambdaCase"
+               , "lib/Data/Aeson/Combinators/Compat.hs"
                , "lib/Data/Aeson/Combinators/Decode.hs"
                , "lib/Data/Aeson/Combinators/Encode.hs"
                ]
