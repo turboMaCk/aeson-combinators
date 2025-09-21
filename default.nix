dc04748b13ad1280eb34e557af64bc916dac43ae
Marek Fajkus <marek.faj@gmail.com>
2025-09-21 11:49:42 +0200

    update ghc

--- default.nix
+++ default.nix
@@ -21,8 +21,8 @@ let
 in with pkgs; {
   ghc = haskellPackages.aeson-combinators;
   ghcjs = ghcjs.pkgs.haskell.packages.ghcjs.callCabal2nix "aeson-combinators" ./. {};
-  ghc884 = haskell.packages.ghc884.aeson-combinators;
-  ghc8107 = haskell.packages.ghc8107.aeson-combinators;
-  ghc902 = haskell.packages.ghc902.aeson-combinators;
-  ghc922 = haskell.packages.ghc922.aeson-combinators;
+  ghc94 = haskell.packages.ghc94.aeson-combinators;
+  ghc96 = haskell.packages.ghc96.aeson-combinators;
+  ghc98 = haskell.packages.ghc98.aeson-combinators;
+  ghc910 = haskell.packages.ghc910.aeson-combinators;
 }
