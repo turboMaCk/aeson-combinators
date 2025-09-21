d95013efba74089dd0b83568c95c4e62feefdf9f
Marek Fajkus <marek.faj@gmail.com>
2025-09-21 12:15:04 +0200

    Iprove

--- test/JSONDecodeSpec.hs
+++ test/JSONDecodeSpec.hs
@@ -10,7 +10,7 @@ import qualified Data.Aeson.Combinators.Decode as JD
 import Data.Aeson.Types (FromJSON (..))
 import Data.ByteString.Lazy
 import Data.ByteString.Lazy.UTF8 (fromString)
-import Data.Text hiding (show)
+import Data.Text (Text)
 import GHC.Generics
 import Test.Hspec
 
